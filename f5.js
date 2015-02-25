0afb695a3046aba51dc4852e9e9e6f113010957e
Erickson Delgado <ericksond@gmail.com>
2015-02-25 12:50:51 -0500

    Modified passwd

--- f5.js
+++ f5.js
@@ -4,12 +4,10 @@ F5.nodeState = function(baseUrl, partition, nodeName) {
 	var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
 
 	if(!Meteor.settings.f5) {
-		console.log('failed to find settings.json')
 		throw new Meteor.Error(500, 'Please configure F5 settings.json')
 	}
 
-	var f5Auth = Meteor.settings.f5.user + ':' +  Meteor.settings.f5.passwd
-	console.log(f5Auth)
+	var f5Auth = Meteor.settings.f5.user + ':' +  Meteor.settings.f5.password
 
 	var f5Response = HTTP.call("GET", url, {
 		timeout: 5000,
@@ -17,7 +15,7 @@ F5.nodeState = function(baseUrl, partition, nodeName) {
 	})
 
 	if(f5Response && f5Response.statusCode === 200) {
-		return f5Response.data.state 
+		return f5Response.data.state
 	} else {
 		throw new Meteor.Error(500, 'F5 call failed with error: ' + f5Response.status_txt)
 	}
