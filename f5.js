9c1c318ea3b1ba8de24ffba790c741c17a9fa776
Erickson Delgado <ericksond@gmail.com>
2015-02-26 15:21:38 -0500

    Refactored nodeStatus

--- f5.js
+++ f5.js
@@ -1,31 +1,37 @@
 F5 = {}
 
-F5.nodeState = function(baseUrl, partition, nodeName) {
-	var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
-
+var f5Initialize = function() {
 	if(!Meteor.settings.f5) {
 		console.log('Cannot find settings.json')
 		throw new Meteor.Error(500, 'Please configure F5 settings.json')
 	}
-
 	if(!Meteor.settings.f5.user)
 		console.log('Missing user parameter in settings.json')
 	if(!Meteor.settings.f5.password)
 		console.log('Missing password parameter in settings.json')
 
-	var f5Auth = Meteor.settings.f5.user + ':' +  Meteor.settings.f5.password
+	return (Meteor.settings.f5.user + ':' +  Meteor.settings.f5.password)
+}
 
-	var f5Response = HTTP.call("GET", url, {
+F5.nodeState = function(baseUrl, partition, nodeName) {
+	var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName + '/stats'
+  var f5Auth = f5Initialize()
+  var f5Response = HTTP.call("GET", url, {
 		timeout: 5000,
 		auth: f5Auth
 	})
 
 	if(f5Response && f5Response.statusCode === 200) {
-		return f5Response.data.state
+    var entries = f5Response.data.entries
+    return entries
 	} else {
 		console.log('F5 call failed with error: ', f5Response.status_txt)
 		throw new Meteor.Error(500, 'F5 call failed with error: ' + f5Response.status_txt)
 	}
+}
+
+F5.poolState = function(baseUrl, partition, poolName) {
+  
 }
 
 Meteor.methods({
