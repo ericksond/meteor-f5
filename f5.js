ff1fcf5b6be606aae503ce25621bf01f2af914e2
Erickson Delgado <ericksond@gmail.com>
2015-03-04 09:23:36 -0500

    Refactored methods

--- f5.js
+++ f5.js
@@ -1,96 +1,93 @@
 F5 = {}
 
-var f5Initialize = function() {
+var init = function(url, method, data) {
 	if(!Meteor.settings.f5) {
 		console.log('Cannot find settings.json')
 		throw new Meteor.Error(500, 'Please configure F5 settings.json')
 	}
+
+  console.log(url, method, data)
+
 	if(!Meteor.settings.f5.user)
 		console.log('Missing user parameter in settings.json')
 	if(!Meteor.settings.f5.password)
 		console.log('Missing password parameter in settings.json')
 
-	return (Meteor.settings.f5.user + ':' +  Meteor.settings.f5.password)
-}
-
-F5.getInstanceStatus = function(baseUrl) {
-  var url = baseUrl + '/mgmt/tm/ltm'
-  var f5Auth = f5Initialize()
-  var f5Response = HTTP.call('GET', url, {
+  var auth = Meteor.settings.f5.user + ':' +  Meteor.settings.f5.password
+  var response = HTTP.call(method, 'https://'+url, {
     timeout: 5000,
-    auth: f5Auth
+    auth: auth,
+    data: data
   })
 
-  if(f5Response && f5Response.statusCode === 200) {
-    return 'up'
-  } else {
-    return 'down'
+  if(response && response.statusCode === 200) {
+    return response
   }
-
+  else {
+    return 'down' 
+  }
 }
 
-F5.getNodeStats = function(baseUrl, partition, nodeName) {
-	var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName + '/stats'
-  var f5Auth = f5Initialize()
-  var f5Response = HTTP.call("GET", url, {
-		timeout: 5000,
-		auth: f5Auth
-	})
+F5.trafficGroupStats = function(instance) {
+  var url = instance + '/mgmt/tm/cm/traffic-group/stats'
+  var response = init(url, 'GET')
+  if (response === 'down')
+    return {status: 'down'}
+  var entries = response.data.entries
+  var keyArray = Object.keys(entries)
 
-	if(f5Response && f5Response.statusCode === 200) {
-    var entries = f5Response.data.entries
-    return entries
-	} else {
-		console.log('F5 call failed with error: ', f5Response.status_txt)
-		throw new Meteor.Error(500, 'F5 call failed with error: ' + f5Response.status_txt)
-	}
-}
-
-F5.forceOffline = function(baseUrl, partition, nodeName) {
-  var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
-  var f5Auth = f5Initialize()
-
-  var f5Response = HTTP.call("PUT", url, {
-    timeout: 5000,
-    auth: f5Auth,
-    data: {
-      state: "user-down",
-      session: "user-disabled"
+  for(var i=0; i < keyArray.length; i++) {
+    var tg = keyArray[i].toLowerCase().split('/')[7].split('~')[4]
+    if (instance == tg) {
+      return {
+        status: 'up',
+        failoverState: entries[keyArray[i]].nestedStats.entries.failoverState.description
+      }
     }
-  })
-
-  if(f5Response && f5Response.statusCode === 200) {
-    //console.log(f5Response)
-    return f5Response 
-  } else {
-    console.log('F5 PUT failed with error: ', f5Response.status_txt)
-    throw new Meteor.Error(500, 'F5 PUT failed with error: ', f5Response.status_txt)
   }
 }
 
-F5.enable = function(baseUrl, partition, nodeName) {
-  var url = baseUrl + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
-  var f5Auth = f5Initialize()
+F5.getInstanceStatus = function(instance) {
+  var url = instance + '/mgmt/tm/ltm'
+  var response = init(url, 'GET')
+  //console.log('response: ', response)
+}
 
-  var f5Response = HTTP.call("PUT", url, {
-    timeout: 5000,
-    auth: f5Auth,
-    data: {
-      state: "user-up",
-      session: "user-enabled"
-    }
-  })
+F5.getAllNodesStats = function(instance) {
+  var url = instance+ '/mgmt/tm/ltm/node/stats'
+  var response = init(url, 'GET')
+  //console.log('response: ', response)
+  return response
+}
 
-  if(f5Response && f5Response.statusCode === 200) {
-    //console.log(f5Response)
-    return f5Response
-  } else {
-    console.log('F5 PUT failed with error: ', f5Response.status_txt)
-    throw new Meteor.Error(500, 'F5 PUT failed with error: ', f5Response.status_txt)
+F5.getNodeStats = function(instance, partition, nodeName) {
+	var url = instance + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName + '/stats'
+  var auth = init()
+  var response = init(url, 'GET')
+  //console.log('response: ', response)
+  return response.data.entries
+}
+
+F5.forceOffline = function(instance, partition, nodeName) {
+  var url = instance + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
+  var data = {
+    state: "user-down",
+    session: "user-disabled"
   }
+  var response = init(url, 'PUT', data)
+  //console.log('response: ', response)
+  return response
 }
 
-F5.getPoolStats = function(baseUrl, partition, poolName) {
-  
+F5.enable = function(instance, partition, nodeName) {
+  var url = instance + '/mgmt/tm/ltm/node/~' + partition + '~' + nodeName
+  var data = {
+    state: "user-up",
+    session: "user-enabled"
+  }
+  var response = init(url, 'PUT', data)
+  //console.log('response: ', response)
+  return response
 }
+
 
